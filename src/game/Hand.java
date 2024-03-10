43634b121ccbe25ad812703801e74798a6827c63
Florencia <fmartinezcarreras@gmail.com>
2024-03-09 22:14:18 -0300

    Added the Hand class to represent poker hands

--- src/game/Hand.java
+++ src/game/Hand.java
@@ -1,4 +1,20 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Hand {
+    private final List<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
+    }
 }
