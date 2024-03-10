4c889cd7ddfe630d54f239f87afcddc969e386d4
Florencia <fmartinezcarreras@gmail.com>
2024-03-09 22:32:49 -0300

    Created the Player class to represent players

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,4 +1,23 @@
 package game;
 
 public class Player {
+    private final String name;
+    private final Hand hand;
+
+    public Player(String name, Hand hand){
+        this.name = name;
+        this.hand = hand;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+    @Override
+    public String toString(){
+        return name + "'s hand" + hand.getCards();
+    }
 }
